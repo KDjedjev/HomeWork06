38a7aeab407c5389af8e95699b835695c4e2f6d2
kokodzh <kdjedjev@gmail.com>
2016-06-04 15:49:39 +0300

    Done

---
+++ src/task03/Main.java
@@ -0,0 +1,11 @@
+package task03;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Meat svinsko = new Meat(23,true,2.4f,"Svinsko");
+	System.out.println(svinsko.available());
+	
+	}
+
+}
